9a3afbc0c9e5240d20fadf3c06cd274616c96ad6
Thiago Tomé <dev.tomethiago@gmail.com>
2023-08-11 22:47:23 -0300

    feat: challenge done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
 import { GetStaticProps } from 'next';
+import { useState, useEffect, ReactElement } from 'react';
+
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { FiUser } from 'react-icons/fi';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import Link from 'next/link';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -22,15 +32,99 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination, preview }: HomeProps) {
+  const [loadedPosts, setLoadedPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  async function handleLoadMore(): Promise<void> {
+    const prismic = getPrismicClient({});
+    const postsResponse = await fetch(nextPage)
+      .then(response => response.json())
+      .then(data => {
+        setNextPage(data.next_page);
+        return data.results;
+      })
+      .catch(err => {
+        console.error(err);
+      });
 
-//   // TODO
-// };
+    const results = postsResponse.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date,
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      };
+    });
+
+    setLoadedPosts([...loadedPosts, ...results]);
+  }
+
+  return (
+    <>
+      <main className={styles.container}>
+        {loadedPosts.map(post => (
+          <div key={post.uid}>
+            <Link href={`/post/${post.uid}`}>
+              <h2>{post.data.title}</h2>
+            </Link>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <span>
+                <AiOutlineCalendar />
+                <span>
+                  {format(
+                    new Date(post.first_publication_date),
+                    'dd MMM yyyy',
+                    { locale: ptBR }
+                  )}
+                </span>
+              </span>
+              <span>
+                <FiUser />
+                <span>{post.data.author}</span>
+              </span>
+            </div>
+          </div>
+        ))}
+        {nextPage && (
+          <button type="button" onClick={handleLoadMore}>
+            Carregar mais posts
+          </button>
+        )}
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts');
+
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results,
+      },
+    },
+  };
+};
